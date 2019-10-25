bf208dae1f3abd40ffe9bc454f79d46f37924515
Mahalingam <kmahalingam@mitre.org>
2019-10-25 14:54:50 -0400

    Add support for POSTing a JSON object to insert an entry into trial_data table

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const {Client} = require('pg');
-const client = new Client();
+const options = require('./knexfile.js');
+const knex = require('knex')(options);
 
-exports.handler = async (event) => {
-  await client.connect();
-  const resp = await client.query('SELECT NOW()');
-  await client.end();
-  return {statusCode: 200, body: {dbTime: resp.rows[0].now}};
+exports.handler = async event => {
+  return knex
+    .insert([JSON.parse(event.body)])
+    .into('trial_data')
+    .then(r => {
+      return { statusCode: 200, body: 'Successfully inserted data.' };
+    })
+    .catch(e => {
+      return { statusCode: 400, body: 'Error uploading data.' };
+    });
 };
