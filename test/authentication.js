13c3bba85307eaaea53c6c857ebd96d1b9b62ca3
Kathy Li <lli@mitre.org>
2019-11-14 10:12:01 -0500

    Working authentication tests up to success case

--- test/authentication.js
+++ test/authentication.js
@@ -7,7 +7,7 @@ const querystring = require('querystring');
 
 describe('Get a valid OAuth Token', async () => {
   const httpsAgent = new https.Agent({
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // Turn off ssl verification
   });
   const hydraAdmin = axios.create({
     httpsAgent,
@@ -22,11 +22,12 @@ describe('Get a valid OAuth Token', async () => {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
   });
+
   const keystore = JWK.createKeyStore();
-  const kid = uuidv4();
+  const kid = uuidv4(); // The jwk's id
   const clientId = 'test-client';
 
-  before(async () => {
+  before(async () => { // Generate a jwk
     await keystore.generate('RSA', 2048, {kid, use: 'sig'});
   });
 
@@ -46,35 +47,41 @@ describe('Get a valid OAuth Token', async () => {
     expect(resp.data).to.deep.include(postData);
   });
 
-  it('should receive a valid token', async () => {
+  it('should receive a valid token for the test client', async () => {
     const private = keystore.get(kid).toJSON(true);
     const tokenEndpoint = '/oauth2/token';
     const options = {
       compact: true,
       alg: 'RS256',
       fields: {
-        kid: uuidv4(),
-        type: 'JWT',
-        iss: clientId,
-        sub: clientId,
-        aud: `${hydraPublic.baseURL}${tokenEndpoint}`,
-        exp: new Date(new Date().getTime() + 300000),
-        jti: uuidv4(),
+        kid,
+        typ: 'JWT',
       },
     };
-    const assertion = await JWS.createSign(options, private).final();
-    try {
-      const resp = await hydraPublic.post(tokenEndpoint, querystring.stringify({
-        client_assertion: assertion,
-        client_assertion_type:
+
+    const assertion = await JWS.createSign(options, private)
+        .update(JSON.stringify({
+          iss: clientId,
+          sub: clientId,
+          aud: `${hydraPublic.defaults.baseURL}${tokenEndpoint}`,
+          exp: new Date().getTime() + 300000,
+          jti: uuidv4(),
+        })).final();
+
+    const resp = await hydraPublic.post(tokenEndpoint, querystring.stringify({
+      client_assertion: assertion,
+      client_assertion_type:
           'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-        grant_type: 'client_credentials',
-        scope: 'system/*.*',
-      }));
-      console.log(resp);
-    } catch (error) {
-      console.log(error);
-    }
+      grant_type: 'client_credentials',
+      scope: 'system/*.*',
+    }));
+
+    expect(resp).to.have.property('data');
+    expect(resp.data).to.have.property('access_token');
+  });
+
+  it('should not receive a token for an invalid client', async () => {
+
   });
 
   after(() => {
