25e1032f0268e2927e8cd7d0341fb8bd2f19d661
Kathy Li <lli@mitre.org>
2019-11-13 21:05:35 -0500

    Initial pass at interfacing with token endpoint

--- test/index.js
+++ test/index.js
@@ -1,49 +1,83 @@
-const chai = require('chai');
+const {expect} = require('chai');
+const https = require('https');
 const axios = require('axios');
 const {JWK, JWS} = require('node-jose');
 const uuidv4 = require('uuid/v4');
+const querystring = require('querystring');
 
 describe('Get a valid OAuth Token', async () => {
-  const hydra = axios.create({
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+  const hydraAdmin = axios.create({
+    httpsAgent,
     baseURL: 'https://localhost:9001',
     headers: {'Accept': 'application/json'},
   });
+  const hydraPublic = axios.create({
+    httpsAgent,
+    baseURL: 'https://localhost:9000',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
   const keystore = JWK.createKeyStore();
   const kid = uuidv4();
+  const clientId = 'test-client';
 
   before(async () => {
     await keystore.generate('RSA', 2048, {kid, use: 'sig'});
   });
 
   it('should register a test client', async () => {
-    const key = keystore.get(kid).toJSON();
+    const public = keystore.get(kid).toJSON();
+    const postData = {
+      client_id: clientId,
+      jwks: {
+        keys: [public],
+      },
+      token_endpoint_auth_method: 'private_key_jwt',
+      scope: 'system/*.*',
+      grant_types: ['client_credentials'],
+    };
+    const resp = await hydraAdmin.post('/clients', postData);
+    expect(resp).to.have.property('data');
+    expect(resp.data).to.deep.include(postData);
+  });
+
+  it('should receive a valid token', async () => {
+    const private = keystore.get(kid).toJSON(true);
+    const tokenEndpoint = '/oauth2/token';
+    const options = {
+      compact: true,
+      alg: 'RS256',
+      fields: {
+        kid: uuidv4(),
+        type: 'JWT',
+        iss: clientId,
+        sub: clientId,
+        aud: `${hydraPublic.baseURL}${tokenEndpoint}`,
+        exp: new Date(new Date().getTime() + 300000),
+        jti: uuidv4(),
+      },
+    };
+    const assertion = await JWS.createSign(options, private).final();
     try {
-      const resp = await hydra.post('/clients', {
-        client_id: 'test-client',
-        jwks: {
-          keys: [key],
-        },
-      });
+      const resp = await hydraPublic.post(tokenEndpoint, querystring.stringify({
+        client_assertion: assertion,
+        client_assertion_type:
+          'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+        grant_type: 'client_credentials',
+        scope: 'system/*.*',
+      }));
       console.log(resp);
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.log(error);
     }
   });
 
-  // it('should receive a valid token', async () => {
-  //   const payload = {};
-  //   const key = await JWK.createKey('RSA', 2048);
-  //   const options = {
-  //     algorithm: 'RS256', // hydra doesn't support any other signing algorithm
-  //     // The following comments are from https://build.fhir.org/ig/HL7/bulk-data/authorization/index.html
-  //     keyid: 'e34bd62e-b02d-4607-b360-afbeaeb986c7', // The identifier of the
-  //     // key-pair used to sign this JWT
-  //     expiresIn: '5m', // This time SHALL be no more than five minutes in the future
-  //     issuer: 'test-client', // the client's client_id
-  //     subject: 'test-client', // this is the same as the value for the iss claim
-  //     audience: 'https://localhost:9000/oauth2/token', // The FHIR authorization server's token URL
-  //     jwtid: 'c08cad54-687f-4b35-90e9-cf108855006e',
-  //   };
-  //   const token = jwt.sign(payload, privateKey, options);
-  // });
+  after(() => {
+    hydraAdmin.delete(`/clients/${clientId}`);
+  });
 });
