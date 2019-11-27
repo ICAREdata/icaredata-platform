c65c7b770e9f282e78ccf4a81dc9779d926f43dc
Mahalingam <kmahalingam@mitre.org>
2019-11-27 13:58:11 -0500

    Clean up tests

--- test/bulkdata.js
+++ test/bulkdata.js
@@ -15,28 +15,6 @@ describe('SMART Configuration endpoint', async () => {
     expect(resp).to.have.property('status');
     expect(resp.status).to.equal(200);
     expect(resp).to.have.property('data');
-    expect(resp.data).to.have.property('token_endpoint');
-    expect(resp.data.token_endpoint).to.equal(statement.token_endpoint);
-    expect(resp.data).to.have.property('token_endpoint_auth_methods_supported');
-    expect(resp.data.token_endpoint_auth_methods_supported.length).to.equal(1);
-    expect(resp.data.token_endpoint_auth_methods_supported[0])
-        .to.equal(statement.token_endpoint_auth_methods_supported[0]);
-    expect(resp.data)
-        .to.have.property('token_endpoint_auth_signing_alg_values_supported');
-    expect(resp.data.token_endpoint_auth_signing_alg_values_supported.length)
-        .to.equal(2);
-    expect(resp.data.token_endpoint_auth_signing_alg_values_supported[0])
-        .to.equal(
-            statement.token_endpoint_auth_signing_alg_values_supported[0]);
-    expect(resp.data.token_endpoint_auth_signing_alg_values_supported[1])
-        .to.equal(
-            statement.token_endpoint_auth_signing_alg_values_supported[1]);
-    expect(resp.data).to.have.property('scopes_supported');
-    expect(resp.data.scopes_supported.length).to.equal(1);
-    expect(resp.data.scopes_supported[0])
-        .to.equal(statement.scopes_supported[0]);
-    expect(resp.data).to.have.property('registration_endpoint');
-    expect(resp.data.registration_endpoint)
-        .to.equal(statement.registration_endpoint);
+    expect(resp.data).to.deep.equal(statement);
   });
 });
