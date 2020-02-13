3030a5a99e178b353e18a16a1ac81c36f8a70ba6
Mahalingam <kmahalingam@mitre.org>
2020-02-13 08:52:05 -0500

    Update message processing to clone successful response object and destroy knex connection on finish

--- process-message/index.js
+++ process-message/index.js
@@ -1,63 +1,42 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-
 const {production} = require('../utils/knexfile.js');
 const responses = require('../utils/responses.js');
 const fhirpath = require('fhirpath');
 const fs = require('fs');
+const cloneDeep = require('lodash/cloneDeep');
 
 exports.handler = async (event) => {
-  const signer = new AWS.RDS.Signer();
-  await new Promise((resolve, reject) => {
-    signer.getAuthToken({
-      region: 'us-east-2',
-      hostname: 'database-2.carn3hm0atrn.us-east-2.rds.amazonaws.com',
-      port: 5432,
-      username: 'lambda',
-    }, (err, token) => {
-      if (err) {
-        console.log(`could not get auth token: ${err}`);
-        reject(err);
-      } else {
-        production.connection.password = token;
-        resolve(token);
-      }
-    });
-  });
-
-  if (!production.connection.password) return responses.response500;
   production.connection.ssl = {
+    rejectUnauthorized: true,
     ca: fs.readFileSync(__dirname + '/../utils/rds-ca-2019-root.pem'),
   };
 
   const knex = require('knex')(production);
 
   // First, verify that this is a message
-  const message = JSON.parse(event.body);
-
   const isMessage =
-    (fhirpath.evaluate(message, 'Bundle.type')[0] === 'message');
+    (fhirpath.evaluate(event, 'Bundle.type')[0] === 'message');
 
   if (!isMessage) return responses.response400;
 
   // Collect the MessageHeader, Parameters, and Patient resources
   // from within the Message
   const messageHeader =
-    getBundleResourcesByType(message, 'MessageHeader', {}, true);
+    getBundleResourcesByType(event, 'MessageHeader', {}, true);
 
   const parameters =
-    getBundleResourcesByType(message, 'Parameters', {}, true);
+    getBundleResourcesByType(event, 'Parameters', {}, true);
 
   const patient =
-    getBundleResourcesByType(message, 'Patient', {}, true);
+    getBundleResourcesByType(event, 'Patient', {}, true);
 
   if (!(messageHeader && parameters && patient)) return responses.response400;
 
 
   // Collect all information that we want to store in the database
   // from the MessageHeader, Parameters, and Patient resources
-  const bundleId = fhirpath.evaluate(message, 'Bundle.id')[0];
+  const bundleId = fhirpath.evaluate(event, 'Bundle.id')[0];
 
   // TODO: Might want some sort of timestamp field from the Bundle eventually
   const timestamp = fhirpath.evaluate(
@@ -86,23 +65,22 @@ exports.handler = async (event) => {
   if (!hasInfo) return responses.response400;
 
   // Now that we've collected the data, format it so we can put in the database
-  const data = {
+  const info = {
     site_id: clinicalTrialSite,
     trial_id: clinicalTrialId,
     mrn: medicalRecordNumber,
     capture_time: timestamp,
     bundle_id: bundleId,
-    bundle: message,
+    bundle: event,
   };
 
-
   // Finally, try to insert the data into the database
-  return knex
-      .insert([data])
+  const response = await knex
+      .insert([info])
       .into('messages')
       .then((r) => {
         // Set the appropriate positive response parameters and send
-        const response200 = responses.response200;
+        const response200 = cloneDeep(responses.response200);
         response200.body.entry[0].resource.response.identifier = bundleId;
         response200.body.entry[0].resource.timestamp =
           new Date().toISOString();
@@ -113,6 +91,10 @@ exports.handler = async (event) => {
         console.log(e);
         return responses.response500;
       });
+
+  knex.destroy();
+
+  return response;
 };
 
 // Utility function to get the resources of a type from our message bundle
