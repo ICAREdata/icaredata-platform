e9ef1895ef326980b9f53034f95cad438ef0f7f7
Rob Dingwell <rob@robdingwell.org>
2020-03-11 11:29:36 -0400

    Fixing issues: Adding X-Forward-Host header to tell keycloak what server it is fronted by.  Fixing bug that did not send over the event body to keycloak.  Addressing issue when querystring parameters were null

--- proxy/index.js
+++ proxy/index.js
@@ -2,22 +2,30 @@ const https = require('https');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const generateQueryUrl = (params) => {
+  if (!params) {
+    return process.env.OAUTH_SERVER_PATH
+  }
   return process.env.OAUTH_SERVER_PATH + '?' + Object.keys(params).map(
-      (key) => key + '=' + params[key],
+    (key) => key + '=' + params[key],
   ).join('&');
 };
 
 exports.handler = async (event) => {
+
   const options = {
     hostname: process.env.OAUTH_SERVER_HOST,
     port: process.env.OAUTH_SERVER_PORT,
     path: generateQueryUrl(event.queryStringParameters),
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'X-Forwarded-Host' : 'testing.icaredata.org'
+    }
   };
 
   return new Promise((accept, reject) => {
     const req = https.request(options, (resp) => {
-      const data = '';
+      let data = '';
       // A chunk of data has been recieved.
       resp.on('data', (chunk) => {
         data += chunk;
@@ -25,13 +33,19 @@ exports.handler = async (event) => {
 
       // The whole response has been received. Print out the result.
       resp.on('end', () => {
-        accept({statusCode: 200, body: JSON.stringify(data)});
+        accept({
+          statusCode: 200,
+          body: JSON.stringify(data)
+        });
       });
     }).on('error', (err) => {
-      accept({statusCode: 200, body: JSON.stringify(err)});
+      accept({
+        statusCode: 200,
+        body: JSON.stringify(err)
+      });
     });
 
-    req.write('');
+    req.write(event.body);
     req.end();
   });
-};
+}
