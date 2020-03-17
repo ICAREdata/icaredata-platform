0a8e82a2f50fc9dd720916189f12f2af7d7b0d31
Mahalingam <kmahalingam@mitre.org>
2020-03-17 07:49:38 -0400

    Dump file from lambda to S3

--- extraction/index.js
+++ extraction/index.js
@@ -1,6 +1,9 @@
 const {getSecret} = require('../utils/getSecret.js');
+const {saveToS3} = require('../utils/saveToS3.js');
 const {production} = require('../utils/knexfile.js');
 const responses = require('../utils/responses.js');
+const exceljs = require('exceljs');
+const Stream = require('stream');
 const fs = require('fs');
 
 exports.handler = async () => {
@@ -14,12 +17,30 @@ exports.handler = async () => {
 
   const knex = require('knex')(production);
 
+  const workbook = new exceljs.Workbook();
+  workbook.creator = 'icaredata';
+  workbook.created = workbook.modified = new Date();
+  const worksheet = workbook.addWorksheet('Data');
+  worksheet.columns = [{header: 'bundle_id', key: 'bundle_id', width: 30}];
+  const stream = new Stream.PassThrough();
+
   const response = await knex
-      .from('messages')
+      .from('data.messages')
       .select('bundle_id')
-      .then((r) => {
+      .then(async (r) => {
         console.log(r);
-        return responses.response200;
+        for (const row of r) {
+          worksheet.addRow(row);
+        }
+        return await workbook.xlsx.write(stream)
+            .then(async () => {
+              await saveToS3(stream, 'icaredata-dev-extracted-data');
+              return responses.response200;
+            })
+            .catch((e) => {
+              console.log(e);
+              return responses.response500;
+            });
       })
       .catch((e) => {
         console.log(e);
@@ -27,5 +48,6 @@ exports.handler = async () => {
       });
 
   knex.destroy();
+
   return response;
 };
