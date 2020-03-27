314b97b3630502ca3d8804962322fdd350ddfbcb
Dan Lee <daniellee@mitre.org>
2020-03-27 11:40:54 -0400

    Fixed eslint errors.

--- extraction/index.js
+++ extraction/index.js
@@ -1,6 +1,6 @@
-const { getSecret } = require('../utils/getSecret.js');
-const { saveToS3 } = require('../utils/saveToS3.js');
-const { production } = require('../utils/knexfile.js');
+const {getSecret} = require('../utils/getSecret.js');
+const {saveToS3} = require('../utils/saveToS3.js');
+const {production} = require('../utils/knexfile.js');
 const responses = require('../utils/responses.js');
 const exceljs = require('exceljs');
 const Stream = require('stream');
@@ -22,86 +22,97 @@ exports.handler = async () => {
   workbook.creator = 'icaredata';
   workbook.created = workbook.modified = new Date();
 
-  // Create separate worksheets for each resource
+  // Create separate worksheets
   const diseaseStatusWorksheet = workbook.addWorksheet('Disease Status');
   diseaseStatusWorksheet.columns = [
-    { header: 'Effective Date', key: 'effectiveDate', width: 30 },
-    { header: 'Cancer Type', key: 'cancerType', width: 30 },
-    { header: 'Coded Value', key: 'codedValue', width: 30 },
-    { header: 'Based On', key: 'basedOn', width: 30 },
-    { header: 'Subject ID', key: 'subjectId', width: 30 },
-    { header: 'Trial ID', key: 'trialId', width: 30 },
-    { header: 'Site ID', key: 'siteId', width: 30 }
+    {header: 'Effective Date', key: 'effectiveDate', width: 30},
+    {header: 'Cancer Type', key: 'cancerType', width: 30},
+    {header: 'Coded Value', key: 'codedValue', width: 30},
+    {header: 'Based On', key: 'basedOn', width: 30},
+    {header: 'Subject ID', key: 'subjectId', width: 30},
+    {header: 'Trial ID', key: 'trialId', width: 30},
+    {header: 'Site ID', key: 'siteId', width: 30},
   ];
 
-  const treatmentPlanChangeWorksheet = workbook.addWorksheet('Treatment Plan Change');
+  const treatmentPlanChangeWorksheet = workbook.addWorksheet(
+      'Treatment Plan Change',
+  );
   treatmentPlanChangeWorksheet.columns = [
-    { header: 'Effective Date', key: 'effectiveDate', width: 30 },
-    { header: 'Coded Value', key: 'codedValue', width: 30 },
-    { header: 'Based On', key: 'basedOn', width: 30 },
-    { header: 'Subject ID', key: 'subjectId', width: 30 },
-    { header: 'Trial ID', key: 'trialId', width: 30 },
-    { header: 'Site ID', key: 'siteId', width: 30 }
+    {header: 'Effective Date', key: 'effectiveDate', width: 30},
+    {header: 'Coded Value', key: 'codedValue', width: 30},
+    {header: 'Based On', key: 'basedOn', width: 30},
+    {header: 'Subject ID', key: 'subjectId', width: 30},
+    {header: 'Trial ID', key: 'trialId', width: 30},
+    {header: 'Site ID', key: 'siteId', width: 30},
   ];
 
   const stream = new Stream.PassThrough();
   const knex = require('knex')(production);
   const response = await knex('data.messages')
-    .select('*')
-    .then(async data => {
-      // Loop through all rows in data.messages
-      data.forEach(d => {
-        const { bundle, subject_id, trial_id, site_id } = d;
+      .select('*')
+      .then(async (data) => {
+        // Loop through all rows in data.messages
+        data.forEach((d) => {
+          const {
+            bundle,
+            subject_id: subjectId,
+            trial_id: trialId,
+            site_id: siteId,
+          } = d;
 
-        const bundleEntry = getBundleResourcesByType(
-          bundle,
-          'Bundle',
-          {},
-          true
-        );
-        console.log('bundle: ', bundleEntry);
-        const dsResources = getBundleResourcesByType(
-          bundleEntry,
-          'Observation',
-          {},
-          false
-        );
-        console.log('dsResources: ', dsResources);
-        dsResources.forEach(resource => {
-          console.log('resource: ', resource);
-          diseaseStatusWorksheet.addRow({
-            effectiveDate: resource.effectiveDateTime,
-            cancerType: resource.focus[0].reference,
-            codedValue: resource.valueCodeableConcept,
-            subjectId: subject_id,
-            trialId: trial_id,
-            siteId: site_id
+          const bundleEntry = getBundleResourcesByType(
+              bundle,
+              'Bundle',
+              {},
+              true,
+          );
+          console.log('bundle: ', bundleEntry);
+          // Get Disease Status resources and add relevant data to worksheet
+          const dsResources = getBundleResourcesByType(
+              bundleEntry,
+              'Observation',
+              {},
+              false,
+          );
+          console.log('dsResources: ', dsResources);
+          dsResources.forEach((resource) => {
+            console.log('resource: ', resource);
+            diseaseStatusWorksheet.addRow({
+              effectiveDate: resource.effectiveDateTime,
+              cancerType: resource.focus[0].reference,
+              codedValue: resource.valueCodeableConcept,
+              subjectId: subjectId,
+              trialId: trialId,
+              siteId: siteId,
+            });
           });
+
+          // Get CarePlan Resources and add data to worksheet
         });
+
+        return await workbook.xlsx
+            .write(stream)
+            .then(async () => {
+              const s3Password = await getSecret('S3-Zip-Password');
+              const archive = archiver.create('zip-encrypted', {
+                zlib: {level: 8},
+                encryptionMethod: 'aes256',
+                password: s3Password.password,
+              });
+              archive.append(stream, {name: 'icare.xlsx'});
+              archive.finalize();
+              await saveToS3(archive, 'icaredata-dev-extracted-data');
+              return responses.response200;
+            })
+            .catch((e) => {
+              console.log(e);
+              return responses.response500;
+            });
+      })
+      .catch((e) => {
+        console.log(e);
+        return responses.response500;
       });
-      return await workbook.xlsx
-        .write(stream)
-        .then(async () => {
-          const s3Password = await getSecret('S3-Zip-Password');
-          const archive = archiver.create('zip-encrypted', {
-            zlib: { level: 8 },
-            encryptionMethod: 'aes256',
-            password: s3Password.password
-          });
-          archive.append(stream, { name: 'icare.xlsx' });
-          archive.finalize();
-          await saveToS3(archive, 'icaredata-dev-extracted-data');
-          return responses.response200;
-        })
-        .catch(e => {
-          console.log(e);
-          return responses.response500;
-        });
-    })
-    .catch(e => {
-      console.log(e);
-      return responses.response500;
-    });
 
   knex.destroy();
 
