e2604736f33eb659fcf587504f5889f536db08c3
Dylan Phelan <dtphelan1@gmail.com>
2020-03-31 15:59:16 -0400

    Added fn comments

--- proxy/index.js
+++ proxy/index.js
@@ -3,24 +3,39 @@ const querystring = require('querystring');
 const _ = require('lodash');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-const generateQueryUrl = (params) => {
+/**
+ * Generate a url joining env-specific server path and provided params .
+ * @param {params} params the request query params.
+ * @return {int} The server-path with our query params properly appended.
+ */
+function generateQueryUrl(params) {
   if (_.isEmpty(params)) {
     return process.env.OAUTH_SERVER_PATH;
   }
   return process.env.OAUTH_SERVER_PATH + '?' + querystring.stringify(params);
 };
 
-exports.handler = async (event) => {
+/**
+ * Generate request options based on env variables and search params.
+ * @param {params} params the request query params.
+ * @return {int} An options object for requests.
+ */
+function generateOptions(params) {
   const options = {
     hostname: process.env.OAUTH_SERVER_HOST,
     port: process.env.OAUTH_SERVER_PORT,
-    path: generateQueryUrl(event.queryStringParameters),
+    path: generateQueryUrl(params),
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       'X-Forwarded-Host': 'testing.icaredata.org',
     },
   };
+  return options;
+};
+
+exports.handler = async (event) => {
+  const options = generateOptions(event.queryStringParameters);
 
   return new Promise((accept, reject) => {
     const req = https.request(options, (resp) => {
