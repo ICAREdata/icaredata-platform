879e89561eab1966f676517e6da5c1fa0ac4805e
Mahalingam <kmahalingam@mitre.org>
2020-05-07 08:05:34 -0400

    Add environment variables so we can vary across environments

--- conformance/index.js
+++ conformance/index.js
@@ -1,5 +1,17 @@
-const conformanceStatement = require('./statement.json');
+const conformanceStatement = {
+  'resourceType': 'Conformance',
+  'url': process.env.METADATA_URL || 'https://testing.icaredata.org/metadata',
+  'version': '0.0.0',
+  'experimental': true,
+  'date': '2019-10-29',
+  'kind': 'capability',
+  'fhirVersion': '1.0.2',
+  'acceptUnknown': 'no',
+  'format': [
+    'application/json+fhir',
+  ],
+};
 
 exports.handler = async () => {
-  return conformanceStatement;
+  return JSON.stringify(conformanceStatement);
 };
