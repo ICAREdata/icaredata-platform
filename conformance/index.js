942150e3e75b6c851a9addefe1a2ef6955272421
Mahalingam <kmahalingam@mitre.org>
2020-09-11 10:23:58 -0400

    Change Conformance to CapabilityStatement and update accordingly

--- conformance/index.js
+++ conformance/index.js
@@ -1,17 +1,17 @@
-const conformanceStatement = {
-  'resourceType': 'Conformance',
+const capabilityStatement = {
+  'resourceType': 'CapabilityStatement',
   'url': process.env.METADATA_URL || 'https://testing.icaredata.org/metadata',
-  'version': '0.0.0',
+  'version': '1.0.0',
+  'status': 'draft',
   'experimental': true,
-  'date': '2019-10-29',
+  'date': '2020-09-11',
   'kind': 'capability',
   'fhirVersion': '4.0.0',
-  'acceptUnknown': 'no',
   'format': [
     'application/json+fhir',
   ],
 };
 
 exports.handler = async () => {
-  return conformanceStatement;
+  return capabilityStatement;
 };
--- test/conformance.test.js
+++ test/conformance.test.js
@@ -5,7 +5,7 @@ describe('Metadata', async () => {
   describe('handler', async () => {
     it('Should return a FHIR conformance statement', async () => {
       const response = await handler();
-      expect(response.resourceType).to.equal('Conformance');
+      expect(response.resourceType).to.equal('CapabilityStatement');
       expect(response.format).to.contain('application/json+fhir');
     });
   });
