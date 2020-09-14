41520d552cc7e07c9f1d0672e0cf0ff32ae12051
Mahalingam <kmahalingam@mitre.org>
2020-09-14 09:48:07 -0400

    Update capabilitystatement based on review

--- conformance/index.js
+++ conformance/index.js
@@ -2,11 +2,11 @@ const capabilityStatement = {
   'resourceType': 'CapabilityStatement',
   'url': process.env.METADATA_URL || 'https://testing.icaredata.org/metadata',
   'version': '1.0.0',
-  'status': 'draft',
+  'status': 'active',
   'experimental': true,
   'date': '2020-09-11',
   'kind': 'capability',
-  'fhirVersion': '4.0.0',
+  'fhirVersion': '4.0.1',
   'format': [
     'application/json+fhir',
   ],
