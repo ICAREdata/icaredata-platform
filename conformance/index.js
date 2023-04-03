8395be908e5938e6089662a36f7b4003cc3d0234
dmendelowitz <79325127+dmendelowitz@users.noreply.github.com>
2023-04-03 09:28:42 -0400

    Updating capability statement (#69)
    * Updating capability statement
    
    * lint fixes
    
    * Adding rest section, parameterizing endpoint address
    
    * Adding process-message operation

--- conformance/index.js
+++ conformance/index.js
@@ -8,6 +8,45 @@ const capabilityStatement = {
   kind: 'capability',
   fhirVersion: '4.0.1',
   format: ['application/json+fhir'],
+  rest: [
+    {
+      mode: 'server',
+      security: {
+        service: [
+          {
+            coding: [
+              {
+                system:
+                  'http://terminology.hl7.org/CodeSystem/restful-security-service',
+                code: 'SMART-on-FHIR',
+              },
+            ],
+          },
+        ],
+      },
+      operation: [
+        {
+          name: 'process-message',
+          definition:
+            'http://hl7.org/fhir/OperationDefinition/MessageHeader-process-message',
+        },
+      ],
+    },
+  ],
+  messaging: [
+    {
+      endpoint: [
+        {
+          protocol: {
+            system: 'http://terminology.hl7.org/CodeSystem/message-transport',
+            code: 'http',
+          },
+          address:
+            process.env.ENDPOINT_ADDRESS || 'https://testing.icaredata.org',
+        },
+      ],
+    },
+  ],
 };
 
 exports.handler = async () => {
